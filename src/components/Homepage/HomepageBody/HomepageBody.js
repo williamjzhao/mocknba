5bd9a40794ee00e329b567c1e7927a0562f7d382
William Zhao <wzhao2017@gmail.com>
2018-07-22 16:00:18 -0500

    functional dropdown menu

--- src/components/Homepage/HomepageBody/HomepageBody.js
+++ src/components/Homepage/HomepageBody/HomepageBody.js
@@ -8,69 +8,75 @@ import teamlogo from '../../../assets/nbalogo.png';
 const logos = require.context('../../../assets/logos',true);
 
 class HomepageBody extends Component {
-  state = {
-    playerImg: playerimage,
-    teamLogo: teamlogo,
-    player: 'Generic Player',
-    playerTeam: 'Harlem Globetrotters',
-    stats: {
-
-    },
-    location: [
-      {
-          id: 0,
-          title: 'New York',
+  constructor(props) {
+    super(props);
+    this.state = {
+      playerImg: playerimage,
+      teamLogo: teamlogo,
+      player: 'Generic Player',
+      playerTeam: 'Harlem Globetrotters',
+      stats: {
+  
+      },
+      activeId: null,
+      activeCity: 'Location',
+      location: [
+        {
+            id: 0,
+            title: 'New York',
+            selected: false,
+            key: 'location'
+        },
+        {
+          id: 1,
+          title: 'Dublin',
           selected: false,
           key: 'location'
-      },
-      {
-        id: 1,
-        title: 'Dublin',
-        selected: false,
-        key: 'location'
-      },
-      {
-        id: 2,
-        title: 'California',
-        selected: false,
-        key: 'location'
-      },
-      {
-        id: 3,
-        title: 'Istanbul',
-        selected: false,
-        key: 'location'
-      },
-      {
-        id: 4,
-        title: 'Izmir',
-        selected: false,
-        key: 'location'
-      },
-      {
-        id: 5,
-        title: 'Oslo',
-        selected: false,
-        key: 'location'
-      }
-    ],
-    playersList: [
-      {
-        id: 0,
-        name: 'Steph Curry',
-        selected: 'false',
-        value: 'curry/steph',
-        key: 'playersList',
-      },
-      {
-        id: 1,
-        name: 'Lebron James',
-        selected: 'false',
-        value: 'james/lebron',
-        key: 'playersList',
-      }
-    ],
+        },
+        {
+          id: 2,
+          title: 'California',
+          selected: false,
+          key: 'location'
+        },
+        {
+          id: 3,
+          title: 'Istanbul',
+          selected: false,
+          key: 'location'
+        },
+        {
+          id: 4,
+          title: 'Izmir',
+          selected: false,
+          key: 'location'
+        },
+        {
+          id: 5,
+          title: 'Oslo',
+          selected: false,
+          key: 'location'
+        }
+      ],
+      playersList: [
+        {
+          id: 0,
+          name: 'Steph Curry',
+          selected: 'false',
+          value: 'curry/steph',
+          key: 'playersList',
+        },
+        {
+          id: 1,
+          name: 'Lebron James',
+          selected: 'false',
+          value: 'james/lebron',
+          key: 'playersList',
+        }
+      ],
+    }
   }
+
 
   getNewAssets = () => {
     fetch('https://nba-players.herokuapp.com/players/curry/stephen')
@@ -83,12 +89,30 @@ class HomepageBody extends Component {
         this.setState({playerImg: newImg, teamLogo: newLogo});
       });
   }
+
+  toggleSelected = (id, key) => {
+    const temp = this.state[key];
+    const active = this.state.activeId;
+    temp[id].selected = !temp[id].selected;
+    if(active != null) {
+      temp[active].selected = !temp[active].selected;
+    }
+    this.setState({
+      [key]: temp,
+      activeId: id,
+      activeCity: temp[id].title,
+    });
+    console.log(temp[id]);
+    console.log(temp[active]);
+  }
+
   render() {
     return (
       <div className={classes.HomepageBody}>
         <PlayerSelector
-          title="Select Player" 
-          list={this.state.location}/>
+          title={this.state.activeCity} 
+          list={this.state.location}
+          toggleItem={this.toggleSelected}/>
         <PlayerDisplay 
           headshot={this.state.playerImg} 
           team={this.state.teamLogo}/>
--- src/components/Homepage/HomepageBody/PlayerSelector/PlayerSelector.js
+++ src/components/Homepage/HomepageBody/PlayerSelector/PlayerSelector.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faAngleDown, faAngleUp, faCheck } from '@fortawesome/free-solid-svg-icons';
 import classes from './PlayerSelector.css';
 
 class PlayerSelector extends Component {
@@ -9,6 +10,10 @@ class PlayerSelector extends Component {
       listOpen: false,
       headerTitle: this.props.title
     }
+  }
+
+  static getDerivedStateFromProps(nextProps){
+      return {headerTitle: nextProps.title};
   }
 
   handleClickOutside() {
@@ -27,20 +32,27 @@ class PlayerSelector extends Component {
     const {list} = this.props;
     const {listOpen, headerTitle} = this.state;
     return(
+      <div className={classes.ok}>
       <div className={classes.dd_wrapper}>
         <div className={classes.dd_header} onClick={() => this.toggleList()}>
             <div className={classes.dd_headertitle}>{headerTitle}</div>
             {listOpen
-              ? <FontAwesomeIcon name="angle-up" size="2x"/>
-              : <FontAwesomeIcon name="angle-down" size="2x"/>
+              ? <FontAwesomeIcon icon={faAngleUp} size="2x"/>
+              : <FontAwesomeIcon icon={faAngleDown} size="2x"/>
             }
         </div>
         {listOpen && <ul className={classes.list}>
           { list.map((item) => (
-            <li className={classes.listitem} key={item.id} >{item.title}</li>
+            <li 
+              className={classes.listitem} 
+              key={item.title} 
+              onClick={() => this.props.toggleItem(item.id, item.key)}>
+                {item.title} {item.selected && <FontAwesomeIcon icon={faCheck}/>}
+            </li>
             ))}
           </ul> 
         }
+      </div>
       </div>
     )
   }
